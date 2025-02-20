1fa90e7e82f5c2d27d2b137f4c446d10576d9e10
yakshipanwar <yakshipanwar1924@gmail.com>
2025-02-20 14:30:39 +0530

    code_1

--- demo2.py
+++ demo2.py
@@ -1,1 +1,15 @@
-print("yakshi")
+class data:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+class college(data):
+    def __init__(self,sname,sage,cname,roll):
+        self.cname=cname
+        self.roll=roll
+        super().__init__(sname,sage)
+    def printdata(self):
+        print("my name is ",self.name," and age is ",self.age,"college : ",self.cname,"roll no.",self.roll)
+
+o1=college("keshav rana",20,"siet",129)
+o1.printdata()
+
---
+++ reverse_a_string.py
@@ -0,0 +1,6 @@
+def reverse_str():
+    str1 = input()
+    return str1[::-1]
+
+print(reverse_str())
+
