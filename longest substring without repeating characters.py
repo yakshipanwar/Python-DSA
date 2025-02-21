60feb6a9ee9e325e6b07efa46b6f44a83d15fd65
yakshipanwar <yakshipanwar1924@gmail.com>
2025-02-21 14:51:13 +0530

    code_10

---
+++ longest substring without repeating characters.py
@@ -0,0 +1,13 @@
+def getdata():
+    str1 = input("enter the string: ")
+    return str1
+
+def substring():
+    a = getdata()
+    b = set(a)
+    return len(b)
+
+
+print(substring())        
+
+    
